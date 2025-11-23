038bf1f92b11e8d276fb2aeef429a4a494fda8bd
AryaStark13 <arihant.sheth0802@gmail.com>
2025-11-23 17:51:25 +0000

    updated script to merge and push checkpoint models

--- script.sh
+++ script.sh
@@ -1,13 +1,14 @@
 python merge_peft_model.py \
---peft_model army-fm-sft-checkpoints/checkpoint-1400 \
+--peft_model ShethArihant/army-fm-sft-checkpoints/checkpoint-1400 \
 --output_dir army-fm-sft-checkpoints/checkpoint-1400
 
 python merge_peft_model.py \
---peft_model army-fm-sft-checkpoints/checkpoint-2200 \
+--peft_model ShethArihant/army-fm-sft-checkpoints/checkpoint-2200 \
 --output_dir army-fm-sft-checkpoints/checkpoint-2200
 
 python merge_peft_model.py \
---peft_model army-fm-sft-checkpoints/checkpoint-2379 \
+--peft_model ShethArihant/army-fm-sft-checkpoints/checkpoint-2379 \
 --output_dir army-fm-sft-checkpoints/checkpoint-2379
 
-hf upload ShethArihant/Llama-3.1-8B-us-army-fm-SFT-instruct-checkpoints army-fm-sft-checkpoints/
+# hf upload ShethArihant/Llama-3.1-8B-us-army-fm-SFT-instruct-checkpoints army-fm-sft-checkpoints/
+hf upload-large-folder ShethArihant/Llama-3.1-8B-us-army-fm-SFT-instruct-checkpoints --repo-type=model army-fm-sft-checkpoints/ --num-workers=16
